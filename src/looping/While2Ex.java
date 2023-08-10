67e7196a1e536c928cbf11454307b95a4a5d6c74
zy_zero <jiyoungjang0307@gmail.com>
2023-08-10 12:18:13 +0900

    while문 내 답과 강사님 답

--- src/looping/While2Ex.java
+++ src/looping/While2Ex.java
@@ -8,27 +8,41 @@ import java.util.Scanner;
 
 public class While2Ex {
   public static void main(String[] args) {
+//    나의 답
+//    while (true) {
+//      System.out.println("동물을 입력해주세요");
+//      Scanner sc = new Scanner(System.in);
+//      String bio = sc.nextLine();
+//      String kind = null;
+//
+//      if(bio.equals("종료")||bio.equals("exit")) {
+//        break;
+//      }
+//
+//      if (bio.equals("호랑이") || bio.equals("사자")) {
+//        kind = "포유류";
+//      } else if (bio.equals("독수리") || bio.equals("참새")) {
+//        kind = "조류";
+//        System.out.printf("%s는 %s입니다.\n", bio, kind);
+//      } else {
+//        System.out.println("입력하신 데이터가 없습니다. 다시 입력하세요.");
+//      }
 
-    while (true) {
-      System.out.println("동물을 입력해주세요");
-      Scanner sc = new Scanner(System.in);
-      String bio = sc.nextLine();
-      String kind = null;
+//      강사님 답
+      Scanner in = new Scanner(System.in);
+      String bio = "", kind ="";
 
-      if(bio.equals("종료")||bio.equals("exit")) {
-        break;
-      }
+      while(true){
+        System.out.println("\n동물의 이름 입력하세요. (종료를 누르면 종료됩니다.)");
+        bio = in.next();
+        if(bio.equals("종료")) break;
 
-      if (bio.equals("호랑이") || bio.equals("사자")) {
-        kind = "포유류";
-      } else if (bio.equals("독수리") || bio.equals("참새")) {
-        kind = "조류";
+        switch (bio) {
+          case "호랑이","사자"-> kind = "포유류";
+          case "독수리","참새"-> kind = "조류";
+          default -> kind = "해당없음";
+        }
         System.out.printf("%s는 %s입니다.\n", bio, kind);
-      } else {
-        System.out.println("입력하신 데이터가 없습니다. 다시 입력하세요.");
-      }
-
-
     }
   }
 }
