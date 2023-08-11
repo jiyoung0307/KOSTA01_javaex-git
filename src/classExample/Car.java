8d0fe9dca0dfcf19654905741bfa1980b1677d1c
zy_zero <jiyoungjang0307@gmail.com>
2023-08-11 10:32:22 +0900

    Car 수정

--- src/classExample/Car.java
+++ src/classExample/Car.java
@@ -1,17 +1,17 @@
 package classExample;
 
 public class Car {
-  int speed = 0;
-  boolean direction = true;
+  int speed=0;
+  boolean direction=true;
 
   public void setSpeed(int speed) {
     this.speed = speed;
   }
 
-  public void setDirection(String gear) {
-    if (gear.equals("전진")) {
+  public void setDirection(String gear){
+    if (gear.equals("전진")){
       this.direction = true;
-    } else if (gear.equals("후진")) {
+    } else if (gear.equals("후진")){
       this.direction = false;
     }
   }
