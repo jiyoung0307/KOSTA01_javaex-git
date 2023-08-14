e57099574b87f42153130f180b2284f0054f116e
zy_zero <jiyoungjang0307@gmail.com>
2023-08-14 11:39:28 +0900

    Day4 배열을 수정해서 length사용으로 코드 반복 줄임

--- src/chap05/ArrayEx.java
+++ src/chap05/ArrayEx.java
@@ -17,18 +17,20 @@ public class ArrayEx {
     sum = 0;
     avg = 0;
 //    compiler가 알아서 int형으로 초기화해줌
-    int[] scores = new int[3];    // 12byte (4 * 3)
+    int[] scores = new int[5];    // 12byte (4 * 3)
     scores[0] = 90;
     scores[1] = 80;
     scores[2] = 100;
+    scores[3] = 100;
+    scores[4] = 90;
 
 //    반복문으로 처리해보자
 //    sum = scores[0] + scores[1] + scores[3];
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < scores.length; i++) {
 //      sum = sum + scores[i];
       sum += scores[i];
     }
-    avg = sum/3;
+    avg = sum / scores.length;
     System.out.printf("sum = %d, avg = %d", sum, avg);
   }
 }
