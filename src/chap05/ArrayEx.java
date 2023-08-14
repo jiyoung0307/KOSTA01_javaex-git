306aef7df75497d9d899b161604e91083c083300
zy_zero <jiyoungjang0307@gmail.com>
2023-08-14 12:15:55 +0900

    Day4 foreach

--- src/chap05/ArrayEx.java
+++ src/chap05/ArrayEx.java
@@ -2,6 +2,14 @@ package chap05;
 
 public class ArrayEx {
   public static void main(String[] args) {
+    /*
+     * 1. 배열 선언
+     * ex) int[] scores;
+     * 2. 참조 변수에 대입
+     * 3. new 작성
+     * ex) score = new in[5];
+     */
+
 //    int sum = 0, avg = 0;
     int sum = 0;
     int avg = 0;
@@ -17,12 +25,27 @@ public class ArrayEx {
     sum = 0;
     avg = 0;
 //    compiler가 알아서 int형으로 초기화해줌
-    int[] scores = new int[5];    // 12byte (4 * 3)
-    scores[0] = 90;
-    scores[1] = 80;
-    scores[2] = 100;
-    scores[3] = 100;
-    scores[4] = 90;
+//    int[] scores = new int[5];    // 12byte (4 * 3)
+//    scores[0] = 90;
+//    scores[1] = 80;
+//    scores[2] = 100;
+//    scores[3] = 100;
+//    scores[4] = 90;
+
+    /* 변수 = 참조하는 값 */
+//    방법1)
+    int[] scores = {90, 80, 100};
+
+//    방법2)
+//    int[] scores = new int[] {90, 80, 100, 100, 90};
+
+//    방법3)
+//    int[] scores;
+//    scores = new int[]{90, 80, 100, 100, 90};
+
+//    방법4) 사용하면 안되는 방법 XXX
+//    int[] scores;
+//    scores = {90, 80, 100, 100, 90};
 
 //    반복문으로 처리해보자
 //    sum = scores[0] + scores[1] + scores[3];
@@ -32,5 +55,22 @@ public class ArrayEx {
     }
     avg = sum / scores.length;
     System.out.printf("sum = %d, avg = %d", sum, avg);
+
+//    크기를 동적으로 늘려보자
+    int[] scoresBig = new int[6];
+    for (int i = 0; i < scores.length; i++) {
+      scoresBig[i] = scores[i];
+    }
+//    scores에 scoresBig의 주소 할당
+    scores = scoresBig;
+    scores[3] = 110;
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println("scores[i] = " + scores[i]);
+    }
+
+//    foreach문
+    for (int score : scores ) {
+      System.out.println(score);
+    }
   }
 }
